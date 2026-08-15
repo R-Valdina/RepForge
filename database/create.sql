f61522b2a3d45423a92b9c340348a8d681d5306c
Falkreon <falkreon@gmail.com>
2026-08-15 11:06:50 -0500

    Stub out sql

---
+++ database/create.sql
@@ -0,0 +1,36 @@
+/* Destroy all existing tables and data in the database */
+DROP DATABASE repforge;
+CREATE DATABASE repforge;
+
+USE repforge;
+
+CREATE TABLE `User` (
+    Id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    DisplayName VARCHAR(40) NOT NULL,
+    PasswordHash VARCHAR(256) NOT NULL,
+    ProfilePicture BINARY NULL
+);
+
+CREATE TABLE `Measurement` (
+    Id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    UserId INT NOT NULL,
+    DateRecorded DATE NOT NULL,
+    Weight FLOAT NULL,
+    Neck FLOAT NULL,
+    UpperArm FLOAT NULL,
+    ForeArm FLOAT NULL,
+    Wrist FLOAT NULL,
+    AcrossBreast FLOAT NULL,
+    UnderBreast FLOAT NULL,
+    Stomach FLOAT NULL,
+    Hips FLOAT NULL,
+    Thigh FLOAT NULL,
+    Calf FLOAT NULL,
+    Ankle FLOAT NULL,
+    BMI FLOAT NULL,
+    BodyFatPercentage FLOAT NULL,
+
+    CONSTRAINT fk_User
+        FOREIGN KEY(UserId)
+        REFERENCES `User`(`Id`)
+);
