99da5e812c5aaa7f3d3a16a84514fc758d3bc446
R-Valdina <r.valdina@gmail.com>
2026-08-15 13:22:42 -0400

    Add mock user test data

--- database/test_data.sql
+++ database/test_data.sql
@@ -1,1 +1,8 @@
- 
+ INSERT INTO User (Id, DisplayName, PasswordHash, ProfilePicture)
+ VALUES 
+(1, 'JordanMiles', '$2y$10$MockHashJordanMiles001', null),
+(2, 'AlexRivera', '$2y$10$MockHashAlexRivera002', null),
+(3, 'TaylorBrooks', '$2y$10$MockHashTaylorBrooks003', null),
+(4, 'MorganReed', '$2y$10$MockHashMorganReed004', null),
+(5, 'CaseyStone', '$2y$10$MockHashCaseyStone005', null);
+
