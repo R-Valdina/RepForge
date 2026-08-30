d805a27020dbd3e45d5631f4db29b3003c46909b
Falkreon <falkreon@gmail.com>
2026-08-30 13:15:52 -0500

    Stub everything out

---
+++ src/database_capability.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+
+// TODO: Declare domain objects
+
+
+
+// TODO: Declare read-only database capability
+
+
+
+// TODO: Declare read-write database capability
+
+?>
---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>This is a page</title>
+</head>
+<body>
+	<h1>Test</h1>
+	<?php
+		echo('<p>It is working.');
+	?>
+</body>
+</html>
